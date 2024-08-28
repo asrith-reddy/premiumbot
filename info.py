416bc649429e1ed5fb6d41b6bd75f2c7752e2d52
asrith-reddy <76733972+asrith-reddy@users.noreply.github.com>
2024-08-28 16:32:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002210254257'))
 MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1001620889031'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
-PORT = os.environ.get('PORT', '5000')
+PORT = os.environ.get('PORT', '5001')
 MAX_BTN = int(environ.get('MAX_BTN', '8'))
 AUTO_DELETE = is_enabled('AUTO_DELETE', True)
 DELETE_TIME = int(environ.get('DELETE_TIME', 300))
